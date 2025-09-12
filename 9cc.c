72a53c32e5bcae57a26946d6c3104ddd2cc8da94
aerialboundaries <takatani.masato@gmail.com>
2025-09-11 22:27:26 -0600

    added : addition and substruction

--- 9cc.c
+++ 9cc.c
@@ -4,13 +4,33 @@
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Incorrect number of argc");
-      return 1;
+    return 1;
   }
+
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "Unexpected strings: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   printf(".section .note.GNU-stack,\"\",@progbits\n");
   return 0;
